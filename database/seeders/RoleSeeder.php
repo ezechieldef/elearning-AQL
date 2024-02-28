9e61c675c8baaf078d8a2557f75fe2dc1dae8491
Vitrack <victordognon@gmail.com>
2024-02-28 07:51:44 +0100

    moi

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -13,8 +14,14 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
+        // Créer des permissions
+        $viewMenuPermission = Permission::create(['name' => 'view menu']);
+        $editSettingsPermission = Permission::create(['name' => 'edit settings']);
         foreach (['PROFESSEUR', "ETUDIANT", "SUPER-ADMIN"] as $value) {
-            Role::create(['name' => $value]);
+            $admin = Role::create(['name' => $value]);
+            $admin->givePermissionTo($viewMenuPermission);
+            $admin->givePermissionTo($editSettingsPermission);
         }
+
     }
 }
